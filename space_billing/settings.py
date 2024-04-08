bc044337433d159e5a9dfe05442578b2d6a1b669
exoutia <jhav365@gmail.com>
2024-04-08 12:51:28 +0530

    fixing the debug issue

--- space_billing/settings.py
+++ space_billing/settings.py
@@ -29,7 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = getenv('DEBUG', default='False')
+# DEBUG = getenv('DEBUG', '').lower() == 'true'
+
+DEBUG = getenv('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = [
     "space-billing.onrender.com",
